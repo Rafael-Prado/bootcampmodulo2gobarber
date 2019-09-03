9bd21d6cbe505b51f9fa7ec4d6ef3c274280cd40
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2019-09-02 21:40:32 -0300

    Adicionado o validação de data e horario no agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -17,6 +18,24 @@ class AppointmentController {
     const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
+
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
 
     if (!isProvider) {
       return res
