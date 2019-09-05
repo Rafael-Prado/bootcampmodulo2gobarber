f62be329181655c44e178bd59b25317b2b992a24
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2019-09-04 22:23:13 -0300

    Customizando templeite de envio de email no node

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -110,6 +110,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
     if (appointment.user_id !== req.userId) {
@@ -131,7 +136,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'agendamento cancelado',
-      text: 'Você tem novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia ' dd 'de ' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
