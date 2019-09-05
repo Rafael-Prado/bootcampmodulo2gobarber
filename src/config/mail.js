912859db379062768b69adc2fcc8c2502397d8d4
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2019-09-04 21:32:13 -0300

    Configurando o envio de email na aplicação

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '9ce599315c4d46',
+    pass: 'c0ee6947987560',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
